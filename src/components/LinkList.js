41fb3a884023eb7b4947d557b74e9abb5f9bcc48
tian <istiancz@gmail.com>
2021-05-12 17:31:49 +0800

    fetch&show linklist

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import Link from './Link';
+import {useQuery, gql} from '@apollo/client';
+
+const FEED_QUERY = gql`
+    {
+        feed {
+            id
+            links {
+                id
+                createdAt
+                url
+                description
+            }
+        }
+    }
+`;
 
 const LinkList = () => {
-  const linksToRender = [
-    {
-      id: '1',
-      description:
-        'Prisma gives you a powerful database toolkit 😎',
-      url: 'https://prisma.io'
-    },
-    {
-      id: '2',
-      description: 'The best GraphQL client',
-      url: 'https://www.apollographql.com/docs/react/'
-    }
-  ];
+  const {data} = useQuery(FEED_QUERY)
 
   return (
     <div>
-      {linksToRender.map((link) => (
-        <Link key={link.id} link={link} />
-      ))}
+      {data && (
+        <>
+          {data.feed.links.map((link) => (
+            <Link key={link.id} link={link} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
