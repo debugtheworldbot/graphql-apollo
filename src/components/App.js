23692b57b9eee4a89fb1f1da2e5c8814f7eec459
tian <istiancz@gmail.com>
2021-05-21 11:17:05 +0800

    pagination

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import LinkList from './LinkList';
 import {Switch, Route} from 'react-router-dom';
 import Login from "./Login";
 import Search from "./Search";
+import {Redirect} from "react-router";
 
 const App = () => {
   return (
@@ -12,14 +13,11 @@ const App = () => {
       <Header />
       <div className="ph3 pv1 background-gray">
         <Switch>
-          <Route exact path="/" component={LinkList} />
-          <Route
-            exact
-            path="/create"
-            component={CreateLink}
-          />
+          <Route exact path="/" render={() => <Redirect to={"new/1"} />} />
+          <Route exact path="/create" component={CreateLink} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/search" component={Search} />
+          <Route exact path="/new/:page" component={LinkList} />
         </Switch>
       </div>
     </div>
